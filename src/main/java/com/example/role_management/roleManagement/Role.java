26cb878b852b68cd35dd1cb2d682d1ca30d99e6c
Abhishek Mallikarjuna <abhishek.mallikarjuna@speridian.com>
2024-09-21 22:37:55 +0530

    feat: create role and user entity

---
+++ src/main/java/com/example/role_management/roleManagement/Role.java
@@ -0,0 +1,27 @@
+package com.example.role_management.roleManagement;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+}
---
+++ src/main/java/com/example/role_management/roleManagement/User.java
@@ -0,0 +1,31 @@
+package com.example.role_management.roleManagement;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+}
